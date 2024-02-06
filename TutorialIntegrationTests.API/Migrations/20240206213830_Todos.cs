f9a618d85e4428fa2763696ac61f36128795c658
Lucas Silva <lukesilva.dev@gmail.com>
2024-02-06 19:05:02 -0300

    Adicionar arquivos de projeto.

---
+++ TutorialIntegrationTests.API/Migrations/20240206213830_Todos.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TutorialIntegrationTests.API.Migrations;
+
+/// <inheritdoc />
+public partial class Todos : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "Todos",
+            columns: table => new
+            {
+                Id = table.Column<string>(type: "text", nullable: false),
+                Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                Description = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                Done = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                CompletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Todos", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Todos");
+    }
+}
---
+++ TutorialIntegrationTests.Tests/ApiFactory.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using TutorialIntegrationTests.API;
+
+namespace TutorialIntegrationTests.Tests;
+
+public class ApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder().WithUsername("postgres")
+                                                                            .WithPassword("postgres")
+                                                                            .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            ConfigureDbContext(services);
+        });
+
+        builder.UseEnvironment("Development");
+        base.ConfigureWebHost(builder);
+    }
+
+    private void ConfigureDbContext(IServiceCollection services)
+    {
+        var context = services.FirstOrDefault(descriptor => descriptor.ServiceType == typeof(TodoContext));
+        if (context != null)
+        {
+            services.Remove(context);
+            var options = services.Where(r => (r.ServiceType == typeof(DbContextOptions))
+              || (r.ServiceType.IsGenericType && r.ServiceType.GetGenericTypeDefinition() == typeof(DbContextOptions<>))).ToArray();
+            foreach (var option in options)
+            {
+                services.Remove(option);
+            }
+        }
+
+        services.AddDbContext<TodoContext>(options =>
+        {
+            options.UseNpgsql(_postgres.GetConnectionString());
+        });
+    }
+
+    public Task InitializeAsync()
+    {
+        return _postgres.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _postgres.StopAsync();
+    }
+}
